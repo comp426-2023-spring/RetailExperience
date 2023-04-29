786ebadd39409e4afff03e769935f7a22c79ae22
Coymw1 <coymw1@ad.unc.edu>
2023-04-29 16:39:02 -0400

    cart, checkout, products
    added products loader, and cart/checkout endpoints

--- index.js
+++ index.js
@@ -14,14 +14,22 @@ app.use(session({
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('js'));
 
-var port = 5005
+var port = 3000
+
+var result = db.prepare(`SELECT COUNT(id) AS count FROM products`).get();
+var productsNumber = result.count;
 
 app.get('/', (req, res, next) => {
+    
+    if (!req.session.cart) {req.session.cart = [];}
     res.status(200).render('home') // send login page html file
+
 })
 
 // Create user endpoint
+
 app.post('/api/profile/', (req, res, next) => {
     let userdata = {
         fname: req.body.fname,
@@ -41,13 +49,13 @@ app.post('/api/profile/', (req, res, next) => {
 
         req.session.loggedin = true;
         req.session.username = userdata.username;
-        req.session.products = {};
-        res.render('products');
         console.log('Logged in.');
 
         console.log('User created.');
-        res.end();
+        res.render('products')
+        //res.redirect('/api/products/');
     }
+    res.end()
 });
 
 app.post('/api/login/', (req, res, next) => {
@@ -59,12 +67,13 @@ app.post('/api/login/', (req, res, next) => {
     if (user) {
         req.session.loggedin = true;
         req.session.username = username;
-        req.session.products = {};
-        res.render('products');
+        res.render('products')
+        //res.redirect('/api/products/');
     } else {
         res.status(401).send('Invalid username or password.');
+        
     }
-    res.end();
+    res.end()
 });
 
 app.get('/api/account/', (req, res, next) => {
@@ -151,7 +160,110 @@ app.post('/api/delete_account/', (req, res, next) => {
 
     res.end();
 });
+
+//load items and display in html
+function loadProducts() {
+
+    var item_html = "";
+    var cont = document.getElementsByClassName('row')
+
+    for(var i = 0; i < 10; i++) {
+        item_html = '<div class="col-6 pt-4">' + '<div class="border border-1 border-dark rounded p-2">' + '<img class="pt-3" src="img/hat.jpg" alt="white cap">' + '<p class="text-center pt-2 fs-4">A</p>' + '<div class="row">' + '<div class="col-7">' + '<p class="text-center fs-5">B</p>' + '</div>' + '<div class="col-5">' + '<button type="button" class="btn btn-outline-success">Buy</button>' + '</div>' + '</div>' + '</div>' + '</div>';
+
+        $(".container" ).append( item_html );
+        
+    }
+}
+
+//renders products page
+app.get('/api/products/', (req, res, next) => {
     
+    let sql = `SELECT * FROM Products;`;
+    let all_products = db.prepare(sql).get();
+    let productCount = db.prepare(`SELECT COUNT(*) as count FROM Products;`).get();
+    var count = productCount.count
+
+
+
+    console.log(all_products)
+    console.log(count)
+    console.log(all_products.name)
+    console.log(all_products.price)
+    console.log(all_products.quantity)
+
+    
+    res.render('products', {"products" : all_products, "productCount" : count, "cart" : req.session.cart});   
+    res.end();
+});
+
+//add product to cart 
+app.post('/api/add_cart/:p', (req, res, next) => {
+    let product_add = db.prepare(`SELECT id, name, price FROM Products WHERE id='${p}';`).get();
+    req.session.cart.push(product_add)
+
+    console.log(product_add)
+    console.log(req.session.cart[0])    
+    
+    res.end();
+});
+
+//remove product from cart
+app.post('/api/remove_product/:p', (req, res, next) => {
+    var product_remove = 
+    
+    if (req.session.cart[p] != null) {
+        let product_remove = db.prepare(`SELECT id, name, price FROM Products WHERE id='${p}';`).get();
+        req.session.cart.remove(product_remove)
+
+    }
+
+    
+    console.log(product_add)
+    console.log(req.session.cart[0])    
+    
+    res.end();
+
+});
+
+//go to cart page
+app.post('/api/cart/', (req, res, next) => {
+    let products = db.prepare(`SELECT name, price, quantity FROM Products;`).get();
+
+    
+    res.render('cart');
+    res.end();
+});
+
+//go to checkout page from cart
+app.post('/api/checkout', (req, res, next) => {
+    let products = db.prepare(`SELECT name, price, quantity FROM Products;`).get();
+
+    res.render('checkout');
+    res.end();
+});
+
+//confirm purchase
+app.post('/api/confirm_purchase', (req, res, next) => {
+    //add interaction to checkout table
+    let get_sql = `SELECT name, price, quantity FROM Products`;
+    let products = db.prepare(get_sql).get();
+
+    //decrement quantity of each item in product db
+    
+
+    //clear cart
+    req.session.products = {};
+
+    //return home
+    res.render('home');
+    //display confirm message
+
+
+    res.end();
+});
+
+
 app.listen(port, () => {
-    console.log("Server listening on port 5005")
+    console.log(`Server listening on port: ${port}`);
+    console.log(`Number of products: ${productsNumber}`);
 })
